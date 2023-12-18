e9f7acaaea3043873cc69c6610928fa7a60817d7
Niranjan Kumar <niranjankumaraina@gmail.com>
2023-12-18 11:09:18 +0530

    uodated post is added

---
+++ components/EditModal.js
@@ -0,0 +1,160 @@
+import React, { useEffect, useState } from "react";
+import {
+  Alert,
+  Modal,
+  StyleSheet,
+  Text,
+  Pressable,
+  View,
+  TextInput,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+
+const EditModal = ({ modalVisible, setModalVisible, post }) => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const navigation = useNavigation();
+
+  // handle update post
+  const updatePostHandler = async (id) => {
+    try {
+      setLoading(true);
+
+      const { data } = await axios.put(`/post/update-post/${id}`, {
+        title,
+        description,
+      });
+      setLoading(false);
+
+      alert(data?.message);
+      navigation.navigate("Home");
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+      alert(error);
+    }
+  };
+
+  // initial post data
+  useEffect(() => {
+    setTitle(post?.title);
+    setDescription(post?.description);
+  }, [post]);
+  return (
+    <View style={styles.centeredView}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Update Your Posts </Text>
+            <Text>Title</Text>
+            <TextInput
+              style={styles.inputBox}
+              value={title}
+              onChangeText={(text) => setTitle(text)}
+            />
+            <Text>Description </Text>
+            <TextInput
+              style={styles.inputBox}
+              value={description}
+              onChangeText={(text) => setDescription(text)}
+              multiline={true}
+              numberOfLines={4}
+            />
+
+            <View style={styles.btnContainer}>
+              <Pressable
+                style={styles.button}
+                onPress={() => {
+                  updatePostHandler(post?._id);
+                  setModalVisible(!modalVisible);
+                }}
+              >
+                <Text style={styles.textStyle}>
+                  {loading ? "Please wait ... " : " update"}{" "}
+                </Text>
+              </Pressable>
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setModalVisible(!modalVisible)}
+              >
+                <Text style={styles.textStyle}>Cancel</Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 35,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  btnContainer: {
+    flexDirection: "row",
+  },
+  button: {
+    borderRadius: 10,
+    padding: 10,
+    elevation: 2,
+    backgroundColor: "black",
+    width: 100,
+    marginVertical: 10,
+    margin: 10,
+  },
+  //   buttonOpen: {
+  //     backgroundColor: "#F194FF",
+  //   },
+  buttonClose: {
+    backgroundColor: "red",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
+  inputBox: {
+    marginBottom: 20,
+    paddingTop: 10,
+    textAlignVertical: "top",
+    backgroundColor: "lightgray",
+    borderRadius: 10,
+    marginTop: 10,
+    paddingLeft: 10,
+  },
+});
+
+export default EditModal;
